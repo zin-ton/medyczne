20a93f8a28cae2ae714cf5091b12b08ed2b45662
zhili <anton.zhylitski@gmail.com>
2025-01-04 17:32:02 +0100

    Almost end

---
+++ medyczne/Models/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace medyczne.Models;
+
+public partial class Patient
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Surname { get; set; } = null!;
+
+    public string Login { get; set; } = null!;
+
+    public string? Password { get; set; }
+}
---
+++ medyczne/Models/Visit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace medyczne.Models;
+
+public partial class Visit
+{
+    public int Id { get; set; }
+
+    public int PatientId { get; set; }
+
+    public int DoctorId { get; set; }
+
+    public DateTime Datetime { get; set; }
+
+    public bool Approved { get; set; }
+}
