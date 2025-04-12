1451201c608804587ca8ca7ea606fb07f264a713
zinton <zhilitskianton05@gmail.com>
2025-04-12 16:28:57 +0200

    CORS policy

--- medyczne/Program.cs
+++ medyczne/Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+string allowedOrigin = "*"; // TODO: Change to your allowed origin
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -7,9 +8,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", policy =>
+    options.AddPolicy("AllowSpecificOrigin", policy =>
     {
-        policy.AllowAnyOrigin()
+        policy.SetIsOriginAllowed(origin => origin == allowedOrigin)
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
